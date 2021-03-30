4eb4b38f35d186c8f4a2a0cd840616735595e304
dafealos <dafealos@hotmail.com>
2021-03-29 22:55:24 -0500

    url

--- script.R
+++ script.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(koboloadeR)
 
+# url  https://kc.kobotoolbox.org/api/v1/
+
+
 install_github("mrdwab/koboloadeR")
 
 kobo_datasets("flisol_2021:flisol_2021") # Ver los proyectos
