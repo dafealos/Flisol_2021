747e219097c7189a319b9f84c324a12ec2404d77
dafealos <dafealos@hotmail.com>
2021-03-29 22:52:45 -0500

    primer

---
+++ script.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(koboloadeR)
+
+install_github("mrdwab/koboloadeR")
+
+kobo_datasets("flisol_2021:flisol_2021") # Ver los proyectos
+
+kobo_submission_count("#codigo") # SAber el numero de observaciones subidos
+
+DF <- kobo_data_downloader("#codigo", "flisol_2021:flisol_2021") # Importat el proyecto
+
+kobo_apps("app_name") # Ejecuta una Shiny app para cargar los datos
+
+DF <- kobo_data_downloader("", "flisol_2021:flisol_2021")
+
+
+
+
